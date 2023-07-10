4a197c92de1f4cfee9bb4f449827dfe5f55c48e9
praveenWebDesigner360 <m.praveenkumar@gmail.com>
2023-07-10 18:37:28 +0530

    Banner Image

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
+"use strict";
+
+// Navigation Variables
 const navigation = document.getElementById("nav");
 const menu = document.getElementById("menu");
 
+// Nav Hamburger
 menu.addEventListener("click", () => {
 	navigation.style.setProperty("--childrenNumber", navigation.children.length);
 
