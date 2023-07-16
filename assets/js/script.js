cde71b1a4c62d22a58d0949968b0c30c2c178813
Praveen Kumar <m.praveenkumar.github@gmail.com>
2023-07-16 22:12:31 +0530

    gallery section

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,34 @@ navigationList.forEach((i) => {
 		menu.classList.toggle("active");
 	});
 });
+
+const model = document.createElement("div");
+model.id = "click-view-gallery";
+document.body.appendChild(model);
+
+const images = document.querySelectorAll(".gallery-img");
+
+// Load all images and click to gallery-img-active
+images.forEach((image) => {
+	image.addEventListener("click", () => {
+		model.classList.add("gallery-img-active");
+		const img = document.createElement("img");
+		// console.log(img);
+		const imgSource = image.src;
+		img.src = imgSource;
+		img.className = "model-img";
+
+		// Remove pervious images repeating
+		while (model.firstChild) {
+			model.removeChild(model.firstChild);
+		}
+		model.appendChild(img);
+		const closeBtn = document.createElement("div");
+		closeBtn.className = "gallery-img-close-btn";
+		model.appendChild(closeBtn);
+	});
+});
+
+model.addEventListener("click", () => {
+	model.classList.remove("gallery-img-active");
+});
